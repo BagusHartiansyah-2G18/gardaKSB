7549e5000847c3e547e181d92d5d525b5436a4fa
BagusHartiansyah@G18 <53298436+BagusHartiansyah-2G18@users.noreply.github.com>
2026-08-22 14:30:59 +0700

    Fix URL path for saving device tokens

--- core/urlAPI.py
+++ core/urlAPI.py
@@ -5,7 +5,7 @@ from core.views import (
     save_device_token,send_pending_notifications
 )
 urlpatterns = [
-    path('device-token/', save_device_token, name='save-device-token'),
+    path('device-tokens/', save_device_token, name='save-device-token'),
     path( "notifications/send-pending/", send_pending_notifications, name="send-pending-notifications", ),
 ] 
 
